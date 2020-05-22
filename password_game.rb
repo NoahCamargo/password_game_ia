d74c7d2be8cb76bc2cb75fd588041f4601757a1a
Noah Camargo <noah.camargo@hotmail.com>
2020-05-22 00:14:23 -0300

    Add Game

--- password_game.rb
+++ password_game.rb
@@ -1,7 +1,44 @@
 class PasswordGame
-  attr_accessor :password
+  TRIES = 10
+  PIECES = 7
 
-  def initialize password
+  attr_accessor :password, :plays
+
+  def initialize *password
     self.password = password
+    self.plays = []
   end
+
+  def play! *password
+    status = check_play password
+
+    self.plays << {
+      status: status,
+      play: password
+    }
+
+    plays
+  end
+
+  def win?
+    password == plays.last[:play]
+  end
+
+  def finished?
+    plays.length >= TRIES || win?
+  end
+
+  private
+
+  def check_play password
+    status = []
+
+    self.password.each_with_index do |piece, i|
+      next status << :black if password[i] == piece
+      status << :white if piece.in? password
+    end
+
+    status
+  end
+
 end
